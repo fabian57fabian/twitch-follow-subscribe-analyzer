8bb2132ae2be67c020ca277b27679505c38b80b1
fabian57fabian <fabian_57@yahoo.it>
2021-09-03 10:31:03 +0200

    Added extra prints on processing

--- find_subscribers.py
+++ find_subscribers.py
@@ -2,27 +2,41 @@ import os
 import argparse
 import numpy as np
 import cv2
+import datetime
 import pandas as pd
 from image_matcher import check_subscribed
+
 
 def find_subscribers(filename: str, template_fn: str):
     template = cv2.imread(template_fn, cv2.IMREAD_COLOR)
     # Open video
     vidcap = cv2.VideoCapture(filename)
+    length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     array_presence = [0]
+
+    # Some helpers for print
+    percent_10 = int(length / 100)  # each 1%
+    # If our template stays for longer, no need to check each frame
+    skip_each_frames = 27
+
     # Start reading
     success, image = vidcap.read()
-    count = 0
+    count, is_subscribe = 0, False
     while success:
-        # Check if template is matched
-        is_subscribe = 1 if check_subscribed(image, template) else 0
-        #Update list
+        if count % percent_10 == 0: print("{}: {}%".format(datetime.datetime.now(), count / percent_10))
+        # process image
+        if count % skip_each_frames == 0:
+            is_subscribe = array_presence[-1]
+        else:
+            # Check if template is matched
+            is_subscribe = 1 if check_subscribed(image, template) else 0
+        # Update list
         array_presence.append(is_subscribe)
         success, image = vidcap.read()
         count += 1
     count -= 1
     array_presence = array_presence[1:]
-    # SAve results to csv
+    # Save results to csv
     df = pd.DataFrame(array_presence)
     df.to_csv(filename + ".csv")
 
@@ -38,3 +52,4 @@ if __name__ == '__main__':
     if not os.path.isfile(args.template):
         print("Template {} does not exist!".format(args.template))
         exit(2)
+    find_subscribers(args.source, args.template)
