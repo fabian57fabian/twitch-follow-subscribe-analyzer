fbbeb12d4d5630198bd1f739ce4d4bf54e88ea5a
fabian57fabian <fabian_57@yahoo.it>
2021-09-03 10:31:39 +0200

    Added cv2 matchtemplate with threashold

--- image_matcher.py
+++ image_matcher.py
@@ -2,5 +2,9 @@ import numpy as np
 import cv2
 
 
-def check_subscribed(image, template, threshold= 0.65):
-    return False
+def check_subscribed(image, template, threshold=0.65):
+    result = cv2.matchTemplate(image[40:140, 550:740, :], template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(result >= threshold)
+    if loc == None:
+        return False
+    return loc[0].shape[0] > 0
